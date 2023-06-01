54db4c77e9297759358efc66ae4c99cf8b6717db
HavaElboim <77002425+HavaElboim@users.noreply.github.com>
2023-06-01 11:14:44 +0300

    optional growth/shrink input value

--- script.js
+++ script.js
@@ -13,20 +13,22 @@ for (i = 0; i < image_array.length; i++) {
   width_array[i] = 100;
 }
 
-function resize(image_num, magnification) {
+function resize(image_num, direction) {
   console.log(
     'resizing image number ',
     image_num,
     ' for image: ',
-    image_array[image_num],
-    'by percent:',
-    magnification
+    image_array[image_num]
   );
+  magnification =
+    document.getElementsByClassName('my_input')[image_num].value || 1.2;
   console.log('original image size is ', image_array[image_num].style.width);
   console.log('original image attributes are ', image_array[image_num].style);
+  console.log('magnification is ', magnification);
   //resizing image[image_num]
   //first get current image size
-  width_array[image_num] *= magnification;
+  if (direction == 'grow') width_array[image_num] *= magnification;
+  else width_array[image_num] /= magnification;
   image_array[image_num].style.width = width_array[image_num] + 'px';
   console.log('new image size is ', image_array[image_num].style.width);
 }
