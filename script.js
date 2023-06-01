ea84e6bf86520f4e6a74ce86264860a88a8a7715
HavaElboim <77002425+HavaElboim@users.noreply.github.com>
2023-06-01 10:00:11 +0300

    4 images, each with bigger/smaller btns enlarge/increase by 10%

--- script.js
+++ script.js
@@ -5,15 +5,28 @@ console.log(
   ' first image is ',
   image_array[0]
 );
+console.log('humber of images: ', image_array.length);
+let width_array = [];
 
-function resize(image_num, percent) {
+for (i = 0; i < image_array.length; i++) {
+  image_array[i].style.width = '100px';
+  width_array[i] = 100;
+}
+
+function resize(image_num, magnification) {
   console.log(
     'resizing image number ',
     image_num,
     ' for image: ',
-    image_array[image_num]
+    image_array[image_num],
+    'by percent:',
+    magnification
   );
+  console.log('original image size is ', image_array[image_num].style.width);
+  console.log('original image attributes are ', image_array[image_num].style);
   //resizing image[image_num]
   //first get current image size
-  image_array[image_num].style.width *= percent;
+  width_array[image_num] *= magnification;
+  image_array[image_num].style.width = width_array[image_num] + 'px';
+  console.log('new image size is ', image_array[image_num].style.width);
 }
