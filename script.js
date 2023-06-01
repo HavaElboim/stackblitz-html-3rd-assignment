1d9864884bd15fff317695053e3649b893849215
HavaElboim <77002425+HavaElboim@users.noreply.github.com>
2023-06-01 09:35:08 +0300

    added resize function to buttons

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
+let image_array = document.getElementsByClassName('my_img');
+console.log(
+  'in script, have image ',
+  image_array,
+  ' first image is ',
+  image_array[0]
+);
 
-function resize(percent) {
-
+function resize(image_num, percent) {
+  console.log(
+    'resizing image number ',
+    image_num,
+    ' for image: ',
+    image_array[image_num]
+  );
+  //resizing image[image_num]
+  //first get current image size
+  image_array[image_num].style.width *= percent;
 }
